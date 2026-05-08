e388b34adbc37eb784e351788ac24832221daff8
mukoko <mukoko@gmail.com>
2026-05-09 05:02:31 +0900

    vesion data

--- build.sh
+++ build.sh
@@ -2,15 +2,30 @@
 # exit on error
 set -o errexit
 
-# Installer les dépendances
+# 1. Installer les dépendances
 pip install -r requirements.txt
 
-# --- AJOUTE CETTE LIGNE ICI ---
-# Créer le dossier pour la base de données s'il n'existe pas
+# 2. Créer le dossier pour la base de données
 mkdir -p data
 
-# Collecter les fichiers statiques
+# 3. Collecter les fichiers statiques (CSS, JS, Images)
 python manage.py collectstatic --no-input
 
-# Appliquer les migrations
+# 4. Appliquer les migrations (Création des tables)
 python manage.py migrate
+
+# 5. CRÉATION AUTOMATIQUE DU COMPTE ADMIN (Ton accès)
+# Remplace 'ton_nom', 'ton_mail@gmail.com' et 'TonMotDePasse123' par tes infos
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = 'hopital@'
+email = 'hopital@gmail.com'
+password = 'hopital@'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f'Superuser "{username}" créé avec succès !')
+else:
+    print(f'Le superuser "{username}" existe déjà.')
+EOF
