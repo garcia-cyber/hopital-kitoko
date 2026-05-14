5f46e2ea4f26d5bead52917747aa671d1af32daa
mukoko <mukoko@gmail.com>
2026-05-15 03:28:54 +0900

    partie receptionniste

--- apps/models.py
+++ apps/models.py
@@ -77,7 +77,7 @@ class Prestation(models.Model):
 class Service(models.Model):
     nom = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
-    date_creation = models.DateTimeField(auto_now_add=True)
+    date_creation = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.nom
--- conf/settings.py
+++ conf/settings.py
@@ -81,7 +81,13 @@ LANGUAGE_CODE = 'fr-fr'
 TIME_ZONE = 'Africa/Kinshasa'  # <--- Très important pour avoir l'heure de Kinshasa
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+USE_TZ = False
+
+# Ajoutez-le juste ici
+DATE_INPUT_FORMATS = [
+    '%d/%m/%Y', # Format jour/mois/année (ex: 15/05/2026)
+    '%Y-%m-%d', # Format standard base de données
+]
 
 # --- FICHIERS STATIQUES (CSS, JS) ---
 STATIC_URL = 'static/'
